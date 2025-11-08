9f386634c2e5cc41a8bd45a52d4732a09705ea6b
Kyle Berry <kylematthewberry@gmail.com>
2025-11-08 19:55:46 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -2,7 +2,12 @@
 
 import { ReactNode } from 'react';
 import { FirebaseProvider } from './provider';
+import { FirebaseAuthProvider } from '@/firebase/auth/auth-provider';
 
 export function FirebaseClientProvider({ children }: { children: ReactNode }) {
-  return <FirebaseProvider>{children}</FirebaseProvider>;
+  return (
+    <FirebaseProvider>
+      <FirebaseAuthProvider>{children}</FirebaseAuthProvider>
+    </FirebaseProvider>
+  );
 }
