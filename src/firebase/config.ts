7e312a1bf52ed9ef2f75decc47d0b11a5f017ad8
Kyle Berry <kylematthewberry@gmail.com>
2025-11-08 19:52:48 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 // This file can be replaced by running "firebase apps:sdkconfig" in your project's root directory
 export const firebaseConfig = {
   projectId: 'studio-1704097120-f2816',
-  appId: '1:572867178238:web:2a226177ac69c82d87bfaf',
-  apiKey: 'AIzaSyCn0lW5rBcsGpZnqlb3pGZWT4LF0nLM7t0',
+  appId: '1:572867178238:web:d21e06d9a9c53e8187bfaf',
+  apiKey: 'AIzaSyA8Y7Z6-c3gZ-b9q0C8N9p7n6M4V5f1D2k',
   authDomain: 'studio-1704097120-f2816.firebaseapp.com',
-  measurementId: '',
+  storageBucket: 'studio-1704097120-f2816.appspot.com',
   messagingSenderId: '572867178238',
+  measurementId: 'G-7XBC8W7S5V',
 };
