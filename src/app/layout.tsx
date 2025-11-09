51c6cc3c6be0a0ac9dc8b46657f4e517c3133873
Kyle Berry <kylematthewberry@gmail.com>
2025-11-09 02:30:23 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 import { cn } from '@/lib/utils';
-import { FirebaseProvider } from '@/firebase/provider';
+import { FirebaseClientProvider } from '@/firebase/client-provider';
 import { BerriesProvider } from '@/hooks/use-berries';
 import { AudioProvider } from '@/hooks/use-audio';
 import MusicPlayer from '@/components/profile/music-player';
@@ -29,7 +29,7 @@ export default function RootLayout({
         />
       </head>
       <body className={cn('font-body antialiased', 'min-h-screen bg-background font-sans')}>
-        <FirebaseProvider>
+        <FirebaseClientProvider>
           <ClientThemeInitializer />
           <BerriesProvider>
             <AudioProvider>
@@ -37,7 +37,7 @@ export default function RootLayout({
               {children}
             </AudioProvider>
           </BerriesProvider>
-        </FirebaseProvider>
+        </FirebaseClientProvider>
         <Toaster />
       </body>
     </html>
